cd13262721e3226fcba0cad35b6f7e1c8e3283fa
84beiroun <8uhipidaser@gmail.com>
2022-07-04 14:20:17 +0600

    Scroll to view issue.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import PosLine from "./components/PosLine";
 import PosData from "./text_repo/positions.json"
 
 function App() {
+    const positions = PosData.sort(function (a, b) {
+        if (a.category.toLowerCase() < b.category.toLowerCase()) return -1;
+        if (a.category.toLowerCase() > b.category.toLowerCase()) return 1;
+        return 0;
+    });
+    const uniquePos = [];
+    positions.forEach(posData => {
+        if (uniquePos.indexOf(posData.category)===-1){
+            uniquePos.push(posData.category)
+        }
+    });
   return (
     <div className="App">
         <header className="App-header">
@@ -19,29 +30,38 @@ function App() {
         </header>
         <nav className="App-nav-bar">
             <div className="Nav-container">
-                <div className="Nav-list">
-                    there will be nav menu
+                <div className="Nav-list">{
+                    uniquePos
+                        .map((uniqueData, i) => <p className="Nav-el" key={i} id={"group" + i}>{uniqueData}</p>)
+                }
                 </div>
             </div>
         </nav>
         <section className="App-content-container">
-            <div className="Positions">
-                {
-                    PosData
-                        .sort(function (a,b) {
-                            if(a.category.toLowerCase() < b.category.toLowerCase()) return -1;
-                            if(a.category.toLowerCase() > b.category.toLowerCase()) return 1;
-                            return 0;
-                        })
-                        .map((posData) => <div key={posData.id}>
-                        <h2>{posData.category}</h2>
-                        <PosLine pos={posData}/>
+            <div className="Positions">{
+                uniquePos
+                    .map((category, i) => <div key={i}>
+                       <h2 className="CategoryName" id={"category" + i}>{category}</h2>
+                        {
+                            positions
+                                .map((posData) =>
+                                {
+                                    if (category === posData.category) {
+                                        return (
+                                        <div key={posData.id}>
+                                            <PosLine pos={posData}/>
+                                        </div>)
+                                    } else {
+                                        return null
+                                    }
+                                })
+                        }
                     </div>)
-                }
+
+            }
             </div>
         </section>
         <footer className="App-footer">
-
         </footer>
     </div>
   );
--- src/components/PosLine.jsx
+++ src/components/PosLine.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import imgPlaceholder from "F:/Programs/Projects/js/alex_v1/src/icons/Placeholder.png"
+import addIcon from "F:/Programs/Projects/js/alex_v1/src/icons/add.png"
 import "F:/Programs/Projects/js/alex_v1/src/styles/App.css"
 const PosLine = (props) => {
+
     return (
         <div className="IndivPos">
             <img src={imgPlaceholder} className="PosImg" alt="position"/>
             <div className="IndivPosData">
                 <div className="IndivPosDataTop">
                     <h3> {props.pos.title} </h3>
-                    <p>{props.pos.body}</p>
+                    <p className="BodyText">{props.pos.body}</p>
                 </div>
                 <div className="IndivPosDataBot">
-                    {props.pos.price}
+                   <p className="PriceText">{props.pos.price}</p>
+                    <img src={addIcon} className="AddIcon" alt="add"/>
                 </div>
             </div>
         </div>
