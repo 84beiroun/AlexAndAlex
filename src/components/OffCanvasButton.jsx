bc82e7d345756bfd1a83d6a2f56c608112d35656
84beiroun <8uhipidaser@gmail.com>
2022-08-13 01:18:47 +0600

    Completed telegram bot integration (without server).

--- src/components/OffCanvasButton.jsx
+++ src/components/OffCanvasButton.jsx
@@ -4,13 +4,37 @@ import Offcanvas from 'react-bootstrap/Offcanvas';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
 import PosOrderCard from './PosOrderCard'
-import {forEach} from "react-bootstrap/ElementChildren";
+import * as creds from '../credentials/credentials'
+
+let buttonPushCounter = 0
 
 function OffCanvasButton({ name, posOrder, ...props }) {
     const [showButton, setShowButton] = useState(false);
 
     const ButtonHandleClose = () => setShowButton(false);
     const ButtonHandleShow = () => setShowButton(true);
+
+    let truePosOrder = []
+    for (const [key, value] of posOrder.posOrder){
+        truePosOrder.push(value)
+    }
+
+    // const posOrderJson = JSON.stringify(truePosOrder);
+    //
+    // const request = {
+    //     method: 'POST',
+    //     mode: 'no-cors',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: posOrderJson
+    // };
+
+    function tgMessage() {
+     // fetch(creds.default.PyServerUrl, request)
+        buttonPushCounter++
+        let message = "Order number: " + buttonPushCounter + "%0a"
+        truePosOrder.map((pos, i) => message += pos.title + "%20" + pos.price + "₽%20X%20" + pos.count + "%0a")
+        fetch(creds.default.BotURL + message)
+    }
 
     return (
         <>
@@ -23,11 +47,15 @@ function OffCanvasButton({ name, posOrder, ...props }) {
                 </Offcanvas.Header>
                 <Offcanvas.Body style={{paddingTop:0, paddingBottom: 0}}>
                     {
-                     Array.from(posOrder.posOrder).map((pos, i) => <PosOrderCard pos={pos[1]} key={i}/>)
+                        truePosOrder.map((pos, i) => <PosOrderCard pos={pos} key={i}/>)
                     //   posOrder.posOrder.map((pos, key) => <PosOrderCard key={key} pos={pos}/>)
                     }
                 </Offcanvas.Body>
-                <div style={{background:"black", height:"200px"}}>a</div>
+                <div style={{background:"lightgray", height:"200px", width:"100%"}}>
+                    <Button onClick={()=>tgMessage()} variant={"outline-primary"}>
+                        something
+                    </Button>
+                </div>
             </Offcanvas>
         </>
     );
